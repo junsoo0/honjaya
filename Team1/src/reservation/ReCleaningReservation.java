46c0af18db6cf603bfc89e22774fdfffbb5da8f8
junsoo0 <1herjs1@gmail.com>
2022-11-27 15:50:34 +0900

    add ReCleaningReservation

---
+++ Team1/src/reservation/ReCleaningReservation.java
@@ -0,0 +1,63 @@
+package reservation;
+
+import java.time.LocalDateTime;
+
+public class ReCleaningReservation {
+    private LocalDateTime signUpTime;
+    private String evidencePhoto;
+    private String reRequestReason;
+    private Boolean isReRequestApproved;
+    private LocalDateTime reRequestCleanDate;
+
+    public void selectReRequestCleanDate() {
+
+    }
+
+    public void reRequestClean() {
+
+    }
+
+    public void approveReRequest() {
+
+    }
+
+    public LocalDateTime getSignUpTime() {
+        return signUpTime;
+    }
+
+    public String getEvidencePhoto() {
+        return evidencePhoto;
+    }
+
+    public String getReRequestReason() {
+        return reRequestReason;
+    }
+
+    public Boolean getReRequestApproved() {
+        return isReRequestApproved;
+    }
+
+    public LocalDateTime getReRequestCleanDate() {
+        return reRequestCleanDate;
+    }
+
+    public void setSignUpTime(LocalDateTime signUpTime) {
+        this.signUpTime = signUpTime;
+    }
+
+    public void setEvidencePhoto(String evidencePhoto) {
+        this.evidencePhoto = evidencePhoto;
+    }
+
+    public void setReRequestReason(String reRequestReason) {
+        this.reRequestReason = reRequestReason;
+    }
+
+    public void setReRequestApproved(Boolean reRequestApproved) {
+        isReRequestApproved = reRequestApproved;
+    }
+
+    public void setReRequestCleanDate(LocalDateTime reRequestCleanDate) {
+        this.reRequestCleanDate = reRequestCleanDate;
+    }
+}
