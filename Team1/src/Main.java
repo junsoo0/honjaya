2207bd50574db6a40a36e8354e554b30b048ac97
Usimth <family582001@gmail.com>
2022-11-29 21:56:05 +0900

    directory 생성 코드 위치 변경

--- Team1/src/Main.java
+++ Team1/src/Main.java
@@ -2,12 +2,7 @@ import account.*;
 import reservation.*;
 import review.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +10,11 @@ public class Main {
         int[] tempCreditInfo = {1,2,3};
         User user = new User("usr1", "01034235643", "kynpook@knu.ac.kr", "1234!!", "경북대 북문", tempCreditInfo,false);
         CleaningReservation cr = new CleaningReservation();
+
+        String path = System.getProperty("user.dir") + File.separator + user.getname();
+        System.out.println("파일 절대 경로 : "+path);
+        File file = new File(path);
+        file.mkdir(); //user name으로 된 directory 생성, (user's dm layer..)
 
         System.out.println("--------------------------------------------------");
         System.out.println("                      혼자야?");
@@ -28,26 +28,9 @@ public class Main {
             System.out.println(user.getname()+"님 예약 하시겠습니까?(Y/N)");
             String answer = in.next();
             if (answer.equals("Y")){
-                String path = System.getProperty("user.dir") + File.separator + user.getname();
-                System.out.println("파일 절대 경로 : "+path);
-                File file = new File(path);
-                if(!file.exists())
-                {
-                    file.mkdir();
-                }
-                else
-                {
-                    System.out.println("DB 생성 오류!!!!!"+user.getname()+"폴더가 이미 존재합니다");
-                }
-
-
                 cr.requestClean();
                 File file2 = new File(file,user.getname()+".txt");
-                BufferedWriter writer = new BufferedWriter(new FileWriter(file2,false));
-                String txt = "absfkldjsaklf;";
-                writer.write(txt);
-                writer.flush();
-                writer.close();
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file2));
             } else if (answer.equals("N")){
                 System.out.println("감사합니다. 안녕히 가세요.");
                 return;
