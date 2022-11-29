21600773f2352b94b740e6c395c8a9281ff163f4
Usimth <family582001@gmail.com>
2022-11-29 21:35:25 +0900

    database 작업 중

--- Team1/src/Main.java
+++ Team1/src/Main.java
@@ -2,6 +2,12 @@ import account.*;
 import reservation.*;
 import review.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
@@ -22,7 +28,24 @@ public class Main {
             System.out.println(user.getname()+"님 예약 하시겠습니까?(Y/N)");
             String answer = in.next();
             if (answer.equals("Y")){
+                String path = System.getProperty("user.dir") + File.separator + user.getname();
+                System.out.println("파일 절대 경로 : "+path);
+                File file = new File(path);
+                if(!file.exists())
+                {
+                    file.mkdir();
+                }
+                else
+                {
+                    System.out.println("DB 생성 오류!!!!!"+user.getname()+"폴더가 이미 존재합니다");
+                }
                 cr.requestClean();
+                File file2 = new File(file,user.getname()+".txt");
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file2,false));
+                String txt = "absfkldjsaklf;";
+                writer.write(txt);
+                writer.flush();
+                writer.close();
             } else if (answer.equals("N")){
                 System.out.println("감사합니다. 안녕히 가세요.");
                 return;
