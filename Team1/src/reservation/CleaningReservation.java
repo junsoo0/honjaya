205598e4ba1a2273332bb4a38916da5569b9b707
junsoo0 <1herjs1@gmail.com>
2022-12-02 14:49:40 +0900

    add userFile

--- Team1/src/reservation/CleaningReservation.java
+++ Team1/src/reservation/CleaningReservation.java
@@ -33,6 +33,8 @@ public class CleaningReservation {
     private ReCleaningReservation   reCleaningReservation;
     private Review                  review;
 
+
+
     public void requestClean() {
         Scanner in = new Scanner(System.in);
 
@@ -325,5 +327,9 @@ public class CleaningReservation {
 
     public FinishCleaningInfo getFinishCleaningInfo() {
         return finishCleaningInfo;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
