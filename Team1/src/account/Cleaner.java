637141c98d26d8f505c8ae0a0dbcd32d6c787895
afterst0p <kingwlgn@gmail.com>
2022-12-05 23:28:52 +0900

    commit review

--- Team1/src/account/Cleaner.java
+++ Team1/src/account/Cleaner.java
@@ -1,6 +1,12 @@
 package account;
 
+import reservation.CleaningReservation;
+
+import java.util.ArrayList;
+
 public class Cleaner extends Account {
+
+    ArrayList<CleaningReservation> cleaningReservation = new ArrayList<CleaningReservation>();
 
     public Cleaner(String name, String phoneNum, String eMail, String password) {
         super(name, phoneNum, eMail, password);
