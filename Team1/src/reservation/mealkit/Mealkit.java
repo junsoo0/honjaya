dd5058236eb1d6261778c4740825aeb10c9c6bc0
afterst0p <kingwlgn@gmail.com>
2022-12-05 18:13:04 +0900

    commit cleaningreservation

--- Team1/src/reservation/mealkit/Mealkit.java
+++ Team1/src/reservation/mealkit/Mealkit.java
@@ -104,13 +104,13 @@ public class Mealkit {
         }
 
         while(true){
-            System.out.print("개수를 입력해주세요.(최대 7개 까지 가능) : ");
+            System.out.print("개수를 입력해주세요(최대 7개까지 가능) : ");
             menuNumber = sc.nextInt();  //메뉴 개수
 
             if(menuNumber > 0 && menuNumber < 8) {
                 break;
             } else {
-                System.out.println("입력한 개수는 "+menuNumber+ "으로 잘못 입력 되었습니다.");
+                System.out.println("입력한 개수는 " +menuNumber+ "으로 잘못 입력 하셨습니다.");
                 System.out.println();
             }
         }
